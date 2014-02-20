d28a18cc50187cf7bff84e33f7a347fa711d88d5
bright-tools <dev@brightsilence.com>
2014-02-20 19:34:19 +0000

    Update to create abstract representation of state-machine from textual version Add some simple nesting

--- src/StateMachine.py
+++ src/StateMachine.py
@@ -1,26 +1,118 @@
-#
-#   @file
-#   @brief Python module supporting the representation of a state
-#          machine
-#     Part of StateTableTools - https://github.com/bright-tools/StateTableTools
-#
-#   @author John Bailey <dev@brightsilence.com>
-#
-#   @copyright Copyright 2014 John Bailey
-#
-#   @section LICENSE
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
+"""
+   @file
+   @brief Python module supporting the representation of a state
+          machine
+     Part of StateTableTools - https://github.com/bright-tools/StateTableTools
 
+   @author John Bailey <dev@brightsilence.com>
+
+   @copyright Copyright 2014 John Bailey
+
+   @section LICENSE
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+class StateMachineRaw:
+    """
+    This class supports the internal represention of the state machine
+    """
+    def __init__(self):
+        self.states = []
+        self.events = []
+        self.transitions = {}
+
+class StateMachine:
+
+    def __init__(self, p_src = None, parent = None ):
+        self.states = {}
+        self.events = []
+        self.transitions = {}
+        self.state_mapping = {}
+        self.transitions_to = 0
+        self.parent = parent
+        if( self.parent is not None ):
+            self.root = parent.root
+        else:
+            self.root = None
+        self.name = "UNNAMED"
+
+        if( p_src is not None ):
+            self.root = self
+            # TODO: Handle nests at a greater depth than 1
+            for state in p_src.states:
+                comps = state.split(",")
+                if( len( comps ) > 1):
+                    target_state = self
+                    for depth in range( 0, len( comps ) ):
+                        state_name = comps[depth].strip()
+                        if( state_name in target_state.states ):
+                            if( not isinstance( target_state.states[ state_name ], StateMachine)):
+                                print "ERROR %s %s!"%(target_state.states[ state_name ], type( target_state.states[ state_name ]))
+                        else:
+                            target_state.states[ state_name ] = StateMachine( parent = target_state )
+                            target_state.states[ state_name ].name = state_name
+
+                        sm = target_state.states[ state_name ]
+                        target_state = sm
+                else:
+                    self.states[ state ] = StateMachine( parent = self )
+                    target_state = self.states[ state ]
+                    target_state.name = state
+                self.state_mapping[ state ] = target_state
+            for state in p_src.states:
+                for trans in p_src.transitions[ state ]:
+                    if( len( p_src.transitions[ state ][ trans ] )):
+                        target = self.state_mapping[ p_src.transitions[ state ][ trans ] ]
+                        self.state_mapping[ state ].transitions[ trans ] = target
+                        target.transitions_to = target.transitions_to + 1
+
+    def getTransitionsTo(self):
+        count = self.transitions_to
+        for state in self.states.values():
+            count = count + state.getTransitionsTo()
+
+        return count
+
+    def resolveNesting(self):
+        # Post-order depth-first traversal of the tree
+        # Do this to ensure that we resolve the nesting on
+        # child nodes so that any commonality in transitions
+        # 'bubbles' up the tree and can be considered in the
+        # routine below
+        for state in self.states.values():
+            state.resolveNesting()
+
+        # Needs to have > 1 sub-state for use to do nesting
+        if( len( self.states ) > 1 ):
+            # Copy the transition list for the first state (could be any state)
+            transitions = self.states[ self.states.keys()[0] ].transitions.copy()
+            # Visit all the states
+            for state in self.states.values():
+                # Check to see if the transitions match those in the snapshot we
+                # took - remove from the snapshot if not
+                for transition in transitions.keys():
+                    if(( transition in state.transitions ) and
+                       ( state.transitions[ transition ] == transitions[ transition ] )):
+                        pass
+                    else:
+                        transitions.pop( transition )
+            # At this point we've effectively filtered the snapshot of
+            # transitions and any that are left are common to all
+            # sub-states
+            for transition in transitions:
+                # Add the transition to our list ...
+                self.transitions[ transition ] = transitions[ transition ]
+                # And remove it from the child states ..
+                for state in self.states.values():
+                    state.transitions.pop( transition )
--- src/StateTable.py
+++ src/StateTable.py
@@ -29,20 +29,67 @@
 import fileinput
 import StateMachine
 
-if __name__ == '__main__':
-    machine = StateMachine.StateMachine()
+from optparse import OptionParser
+
+def output_dot_clusters( machine ):
+    for state in machine.states.values():
+        if( len( state.states )):
+            # style = dashed; 
+            print "subgraph \"cluster%s\" { label=\"%s\";"%(state.name,state.name)
+            for sub_state in state.states.values():
+                if( len( sub_state.states )):
+                    output_dot_clusters( state )
+                else:
+                    print "  \"%s\""%(sub_state.name)
+            print "}"
+
+def output_dot_state( machine ):
+    for state in machine.states.values():
+        output_dot_state( state )
+        if( len( state.states ) == 0):
+            print "\"%s\" [ shape = box ];"%(state.name)
+        for trans in state.transitions:
+            src_name = state.name
+            attrs = ""
+            if( len( state.states )):
+                src_name = state.states[ state.states.keys()[0] ].name
+                attrs = " ltail=\"cluster%s\""%(state.name)
+            dest = state.transitions[ trans ].name
+            # TODO: deal with escaping of the label & node names
+            print "\""+src_name + "\"->\"" + dest + "\" [ dir=forward taillabel=\""+trans+"\"%s];"%(attrs)
+            # decorate = true?
+
+def output_dot( machine ):
+    print "digraph G {"
+    print "rankdir=LR;"
+# USE THESE FOR FDP?
+#    print "nodesep=2.0"
+#    print "sep = 0.6;"
+#    print "splines = ortho;"
+# END FDP
+#    print "K=0.2;"
+#    print "overlap = vpsc;"
+#    print "splines = curved;"
+#    print "splines = true;"
+    print "compound=false;"
+    output_dot_clusters( machine )
+    output_dot_state( machine )
+    print "}"
+
+def parse_sem( p_input ):
+    machine = StateMachine.StateMachineRaw()
     first = True
-    for line in fileinput.input():
+    for line in p_input:
         components = line.split( '|' )
         if first:
             for event in components[1:]:
                 evt = event.strip()
                 machine.events.append( evt )
-#                print "Added Event: "+evt
+#            print "Added Event: "+evt
             first = False
         else:
             state = components[0].strip()
-            machine.states.append( state )
+            machine.states.append( state );
             if( state in machine.transitions ):
                 print "Error: Repeated state %s"(state)
             else:
@@ -52,17 +99,39 @@ if __name__ == '__main__':
                 trans = transition.strip()
                 machine.transitions[ state ][ machine.events[ trans_ctr ] ] = trans 
                 trans_ctr = trans_ctr + 1
+    return machine
 
-    # TODO: Check referential integrity of machine
-    # TODO: Change transitions to be in tuples
+input_formats= {"sem": parse_sem }
+output_formats= {"dot": output_dot }
 
-    print "digraph G {"
-    for state in machine.states:
-        for trans in machine.transitions[ state ]:
-            dest = machine.transitions[ state ][trans]
-            if( len( dest )):
-                # TODO: deal with escaping of the label & node names
-                print "\""+state + "\"->\"" + dest + "\" [ dir=forward label=\""+trans+"\"]"
-    print "}"
+if __name__ == '__main__':
+    parser = OptionParser()
+    opts_ok = True
+    parser.add_option("-s", "--source-format",
+                      action="store",
+                      metavar="FORMAT",
+                      dest="sourceformat",
+                      help="format of the table",
+                      default="sem" )
+    parser.add_option("-d", "--dest-format",
+                      action="store",
+                      metavar="FORMAT",
+                      dest="destformat",
+                      help="format of the output",
+                      default="dot" )
+    (options, args) = parser.parse_args()
+    if( options.sourceformat not in input_formats.keys() ):
+        print "ERROR: source format %s is not understood"
+        opts_ok = False
+    if( options.destformat not in output_formats.keys() ):
+        print "ERROR: destination format %s is not understood"
+        opts_ok = False
 
+    if opts_ok:
+        machine = input_formats[options.sourceformat]( fileinput.input(args) )
 
+        abstractMachine = StateMachine.StateMachine( machine )
+        # TODO: Check referential integrity of machine
+        # TODO: Transition actions
+        abstractMachine.resolveNesting()
+        output_formats[options.destformat]( abstractMachine )
